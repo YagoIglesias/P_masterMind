a36c569b137d1e3b5ddcd13af924cee26d0d3e05
Yago Iglesias <pw17kto@eduvaud.ch>
2023-09-15 15:46:30 +0200

    Test tries

--- P_masterMind/Program.cs
+++ P_masterMind/Program.cs
@@ -24,29 +24,54 @@ namespace masterMinde
             string black = "Noir";
             string white = "Blanc";
             string magenta = "Magenta";
+            //Var pour conter les essais
+            int tries = 0;
+            //var Code utilisateur
+            string answer = "";
            
             //creation du code 4 couleurs
             Random code = new Random();
             
             //dans color on à la liste des couleurs
             string[] color = {red, green, blue, orange, black, white, magenta};
-            //créer un numero alletoire entre 0-7
+            //créer un numero alletoire entre 0-6
             int c1 = code.Next(color.Length);
             int c2 = code.Next(color.Length);
             int c3 = code.Next(color.Length);
             int c4 = code.Next(color.Length);
 
             //test pour voir la création du code
-            /*Console.WriteLine("Le code couleur est : ");
-            Console.WriteLine($"{color[c1]} {color[c2]} {color[c3]} {color[c4]}");*/
+            Console.WriteLine("Le code couleur est : ");
+            Console.WriteLine($"{color[c1]} {color[c2]} {color[c3]} {color[c4]}");
 
             //Informations du jeu mistermind
             Console.WriteLine("Bienvenue sur Mastermind!");
             Console.WriteLine("Devine le code en 4 couleurs");
             Console.WriteLine("Couleurs possibles: Rouge, Bleu, Vert, Noir, Orange, Blanc, Magenta");
-            Console.WriteLine("Vous avez 10 essais pour reussir");
+            Console.WriteLine("Vous avez 10 essais pour reussir\n");
+
+            //Boucle pour conter le nombre d'essais
+            do
+            {
+                tries++;
+                Console.WriteLine($"Essai {tries}");
+                answer = Console.ReadLine();
+
+                if (answer != color[c1] || answer != color[c2] || answer != color[c3] || answer != color[c4])
+                {
+                    Console.WriteLine("Auccune couleur trouvée");
+
+                }
+                else if (answer == color[c1] || answer != color[c2] || answer != color[c3] || answer != color[c4])
+                {
+                    Console.WriteLine($"{color[c1]} OK");
+                }
+                
 
 
+            } while (tries <= 9);
+
+            
 
 
 
